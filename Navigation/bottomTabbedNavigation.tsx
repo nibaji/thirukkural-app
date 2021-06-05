a0545d16a44b85196ac3dec720194e3fa347f303
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2021-06-05 23:59:38 +0530

    Add icons and change colors for tabs

--- Navigation/bottomTabbedNavigation.tsx
+++ Navigation/bottomTabbedNavigation.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { FontAwesome, Ionicons, Octicons } from "@expo/vector-icons";
 import aram from "../Screens/aram";
 import porul from "../Screens/porul";
 import kaamam from "../Screens/kaamam";
-import { NavigationContainer } from "@react-navigation/native";
+import AppColor from "../Theme/colors";
 
 export default function BottomTabbedNavigation() {
   return (
@@ -17,10 +19,41 @@ const BottomTab = createMaterialBottomTabNavigator();
 
 function BottomTabs() {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen name="Arathuppaal" component={aram} />
-      <BottomTab.Screen name="Porutpaal" component={porul} />
-      <BottomTab.Screen name="Kaamathuppaal" component={kaamam} />
+    <BottomTab.Navigator
+      initialRouteName="Arathuppaal"
+      activeColor={AppColor.accent}
+      barStyle={{ backgroundColor: AppColor.primary }}
+    >
+      <BottomTab.Screen
+        name="Arathuppaal"
+        component={aram}
+        options={{
+          tabBarLabel: "அறத்துப்பால்",
+          tabBarIcon: ({ color }) => (
+            <Octicons name="law" size={24} color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Porutpaal"
+        component={porul}
+        options={{
+          tabBarLabel: "பொருட்பால்",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="rupee" size={24} color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Kaamathuppaal"
+        component={kaamam}
+        options={{
+          tabBarLabel: "காமத்துப்பால்",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="male-female-outline" size={24} color={color} />
+          ),
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
---
+++ Theme/colors.ts
@@ -0,0 +1,6 @@
+const AppColor = {
+  primary: "#30567a",
+  accent: "#d2e3f0",
+};
+
+export default AppColor;
