ad7e605290c4805665793b64d69ddb2f5e37f29e
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2021-06-07 00:58:58 +0530

    set statusbar color

--- App.tsx
+++ App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 import TopTabbedNavigation from "./Navigation/topTabbedNavigation";
+import { StatusBar } from "react-native";
+import AppColor from "./Theme/colors";
 
 export default function App() {
   return (
     <Provider store={store}>
+      <StatusBar backgroundColor={AppColor.primary} />
       <TopTabbedNavigation />
     </Provider>
   );
